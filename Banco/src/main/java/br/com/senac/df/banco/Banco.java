1c9ece56f865fef46c1ce6625692004e042e6d17
Francisco Vasconcelos <frvasconcelos7@gmail.com>
2026-09-16 17:04:59 -0300

    Atualizado

--- Banco/src/main/java/br/com/senac/df/banco/Banco.java
+++ Banco/src/main/java/br/com/senac/df/banco/Banco.java
@@ -16,5 +16,12 @@ public class Banco {
         conta1.depositar(100);
         conta1.sacar(10);
         conta1.extratoBancario();
+        
+        System.out .println(conta1.getTitular());
+        System.out.println(conta1.getSaldo());
+        
+        conta1.setTitular("Francisco Wallyson");
+        
+        System.out.println(conta1.getTitular());
     }
 }
--- Banco/src/main/java/br/com/senac/df/banco/ContaBancaria.java
+++ Banco/src/main/java/br/com/senac/df/banco/ContaBancaria.java
@@ -17,6 +17,17 @@ public ContaBancaria(String titular){
     this.saldo = 0.00;  
   }  
 
+public  String getTitular(){
+    return this.titular;
+}
+
+public double getSaldo(){
+    return this.saldo;
+}
+public void setTitular(String Titular){
+    this.titular = titular;
+}
+
 public void depositar(double valor){
     if(valor > 0){
         this.saldo = this.saldo + valor;
